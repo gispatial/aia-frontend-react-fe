2bdae1e2a55547f34d2303a793586bdc35be69b0
Just Ed <aidy@rizq.com.my>
2021-10-13 19:05:44 +0800

    push_two

---
+++ build/precache-manifest.086c612db2bd38cc0ac6feb434f35dc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4dcb930baf1205d96196bfa6637071f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf70df814b55d91c58aa",
+    "url": "/static/css/2.ad278b68.chunk.css"
+  },
+  {
+    "revision": "0c63acd284b44d8306aa",
+    "url": "/static/css/main.7a973784.chunk.css"
+  },
+  {
+    "revision": "bf70df814b55d91c58aa",
+    "url": "/static/js/2.1c44f1c2.chunk.js"
+  },
+  {
+    "revision": "0c63acd284b44d8306aa",
+    "url": "/static/js/main.a0ac510b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f1178efdec72ad5bab45a5a546b9979f",
+    "url": "/static/media/logo.f1178efd.png"
+  }
+]);
